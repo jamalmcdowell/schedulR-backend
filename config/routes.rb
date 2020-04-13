55793c66de134d0b307185fcda6c07bbbd9d5317
Jamal McDowell <jamal.mcdowell@gmail.com>
2020-04-13 17:01:23 -0400

    removing lockfile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
     root 'events#index'
   resources :users
   resources :events
-  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'application/json' }]}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
