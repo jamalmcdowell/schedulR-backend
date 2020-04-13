36d0b94e7f641f1d5daa07014ce7a4f7cf1efa42
Jamal McDowell <jamal.mcdowell@gmail.com>
2020-04-13 16:57:52 -0400

    removing lockfile

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://schedul-r.herokuapp.com/', 'http://localhost:8080'
+    origins 'https://schedul-r.herokuapp.com/'
 
     resource '*',
       headers: :any,
